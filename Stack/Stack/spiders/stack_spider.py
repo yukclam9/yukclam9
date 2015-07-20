183449f44629c16ddf6f25aea7bf25c78967b3f0
yukclam9 <yukclam9@gmail.com>
2015-07-20 22:36:43 +0800

    My first spider

---
+++ Stack/Stack/spiders/stack_spider.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 19 16:06:42 2015
+
+@author: casey
+"""
+from scrapy.crawler import Crawler
+from scrapy import Spider
+from scrapy.selector import Selector
+from Stack.items import StackItem
+
+class StackSpider(Spider):
+    name ="teststack"
+    allowed_domains = ["stackoverflow.com"]
+    staturls= ["http://stackoverflow.com/questions?pagesize=50&sort=newest",
+              ]
+              
+    def parse(self,response):
+        # grab all h3 element thar are the children of div and having the class= summary
+        questions= Selector(response).xpath('//div[@class="summary"]/h3')    
+        
+        for question in questions:
+            item= StackItem()
+            ## under h3, there is a class called question-hyperlink 
+            ## and text() should be some standard syntax
+            item['title'] = question.xpath(
+                'a[@class="question-hyperlink"]/text()').extract()[0]
+            item['url']   = question.xpath(
+                'a[@class="question-hyperlink"]/@href').extract()[0]
+            yield item
+
+#https://realpython.com/blog/python/web-scraping-with-scrapy-and-mongodb/
