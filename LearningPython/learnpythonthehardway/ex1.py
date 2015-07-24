dd9e9481d31c37250dd680976877d1e93240c92f
yukclam9 <yukclam9@gmail.com>
2015-07-25 03:40:14 +0800

    Updated on 25/7/2015

---
+++ LearningPython/learnpythonthehardway/ex1.py
@@ -0,0 +1,15 @@
+# the use of '#' is to comment a code and it would not be executed ( by python?)
+# the use of single quote and double quote are interchangeable in a sense that 
+# we use another type of quote to differentiate 'level' of expression
+
+# " I 'said' do not touch this"
+# ' I "said" do not touch this'
+
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this"
+print "this is fun."
+print 'Yay! Printing.'
+print " I would much rather you'not'."
+print ' I "said" do not touch this. '
---
+++ LearningPython/learnpythonthehardway/ex3.py
@@ -0,0 +1,30 @@
+print "i will now count my chickens:"
+
+print "Hens ", 25 + 30 /6
+print "Roosters", 100 - 25*3%4
+
+print "Now i will count the eggs:"
+
+# where x%y return the remainder of the divison
+print 3+2+1-5+4%2-1/4+6
+
+
+print " is it true that 3+2 < 5-7 ?"
+
+# numbers alongsider operator would conduct all possible numeric operations
+# before undergoing the boolean operator
+print 2+2+23+2<5-7*3
+
+print "What is 3+2?", 3+2
+print "what is 5-7?", 5-7
+
+print "Oh, that's why it is False."
+
+print " How about some more."
+
+print " is it greater ?", 5>-2
+print "Is it greater or equal?", 5 >= -2
+print " is it less or equal", 5<=-2
+
+# numbers with type int would drop the decimal while it is not the case for
+# floating point
